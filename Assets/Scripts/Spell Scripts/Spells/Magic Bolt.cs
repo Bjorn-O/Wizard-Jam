bdf892e0d948f1638fed8ca7b9b2139de4710715
Bjorn 'Emil' Oudkerk <Bjorn.oudkerk@outlook.com>
2023-11-17 18:46:15 +0100

    Intermin Push

--- Assets/Scripts/Spell Scripts/Spells/Magic Bolt.cs
+++ Assets/Scripts/Spell Scripts/Spells/Magic Bolt.cs
@@ -1,25 +1,11 @@
-using DG.Tweening;
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class MagicBolt : Spell
 {
     [SerializeField] private Transform castPoint;
     [SerializeField] private Transform playerPoint;
-    [SerializeField] private int castSpreadDegrees;
-    public AnimationCurve curve;
-
-    private void Start()
-    {
-        curve.ClearKeys();
-        curve.AddKey(-2, 0);
-        curve.AddKey(0, 1f);
-        curve.AddKey(2, 0);
-    }
+    [SerializeField] private AnimationCurve curve;
 
     private void Update()
     {
@@ -56,35 +42,44 @@ public class MagicBolt : Spell
             float shotPosition = 0 - Mathf.Floor(amount / 2);
             for (int i = 0; i < amount; i++)
             {
+                // Skips the 0 on even number of shots
                 if (amount % 2 == 0 && shotPosition == 0) shotPosition++;
 
+                // Sets the facing of each shot so they will spread out according to the Animation Curve
                 Vector3 shotDirection = new Vector3(shotPosition / 10, castPoint.transform.position.y, curve.Evaluate(shotPosition / 10));
                 Vector3 shotPoint = transform.position + shotDirection;
-
                 float radial = Mathf.Atan2(transform.position.x, transform.position.z) - Mathf.Atan2(shotPoint.x, shotPoint.z);
                 float degrees = radial * (180 / Mathf.PI);
 
-                SpellEffect bolt = Instantiate(effect, castPoint.transform.position, castPoint.rotation);
+                //Spawn Spell and set position/rotation
+                SpellEffect bolt = spellEffectPool.Get();
+                bolt.transform.position = castPoint.position;
                 bolt.transform.Rotate(0, degrees, 0);
+                
+                //Apply Spell stats to object
+                bolt.transform.localScale *= effectScale;
+                bolt.damage = damage;
 
                 shotPosition++;
             }
         }
     }
 
-    private Vector3 CalculateShotPoint(float radial)
-    {
-        Vector3 originPoint = new Vector3(castPoint.position.x - castPoint.localPosition.x,
-            castPoint.position.y,
-            castPoint.position.z - castPoint.localPosition.z);
-        Vector3 castDir = castPoint.position - originPoint;
-        Vector3 rotatedDirection = castDir * radial;
-        return castPoint.position + rotatedDirection;
-    }
-    private Vector3 GetPlayerPoint()
-    {
-        return new Vector3(castPoint.position.x - castPoint.localPosition.x,
-            castPoint.position.y,
-            castPoint.position.z - castPoint.localPosition.z);
-    }
+    //Decrapit
+
+    //private Vector3 CalculateShotPoint(float radial)
+    //{
+    //    Vector3 originPoint = new Vector3(castPoint.position.x - castPoint.localPosition.x,
+    //        castPoint.position.y,
+    //        castPoint.position.z - castPoint.localPosition.z);
+    //    Vector3 castDir = castPoint.position - originPoint;
+    //    Vector3 rotatedDirection = castDir * radial;
+    //    return castPoint.position + rotatedDirection;
+    //}
+    //private Vector3 GetPlayerPoint()
+    //{
+    //    return new Vector3(castPoint.position.x - castPoint.localPosition.x,
+    //        castPoint.position.y,
+    //        castPoint.position.z - castPoint.localPosition.z);
+    //}
 }
